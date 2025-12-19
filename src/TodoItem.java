bbcdeb7e122891c9b6dd99aa6809b9c6ccf537e1
Yjeongmo <jm616mj@gmail.com>
2025-12-19 18:05:06 +0900

    todoItem : 각 리스트 관리

---
+++ src/TodoItem.java
@@ -0,0 +1,41 @@
+public class TodoItem {
+
+    // 리스트 ID
+    private int id;
+    // 리스트 내용
+    private String content;
+    // 수행 여부
+    private boolean isCompleted;
+
+    // 생성자. ID, 내용, 수행 여부
+    public TodoItem(int id, String content, boolean isCompleted) {
+        this.id = id;
+        this.content = content;
+        this.isCompleted = isCompleted;
+    }
+
+    // 내용을 변경하는 메서드
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // 완료 상태를 변경하는 메서드
+    public void setCompleted(boolean completed) {
+        this.isCompleted = completed;
+    }
+
+    // 저장할 테이터 변환
+    public String toFileFormat() {
+        return id + "," + content + "," + isCompleted;
+    }
+
+    // Getter
+    public int getId(){return id;}
+    public String getContent(){return content;}
+    public boolean isCompleted(){return isCompleted;}
+
+    @Override
+    public String toString() {
+        return "[" + (isCompleted ? "V" : " ") + "] " + id + ". " + content;
+    }
+}
